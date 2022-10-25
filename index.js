fda98d39baa94204757b241b239aa9656426ddc4
dcristobal-sf <44408128+dcristobal-sf@users.noreply.github.com>
2022-10-25 13:26:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ express()
   .get('/', (req, res) => 
        {
         res.locals.title = 1234
-        res.locals.token = 'QTA5MDAwMDAwRHpzMnJTR1hTc1p4dkJHSXpvNTRVeDlUSGxOSXZhaXVLWHR1TCtYa0FaSk5FMTREN29Mb3hCM3hXVTQ3L2NWdmdQTlozQkgrc2hqZ3M4N1lTZ1lWTUYvMkFMN0ZyNTU4RXEzM3k5UXZBYVdhWUpka0NNbnpNL3dVc0JHNWF4c1pWNmhZSXdnQlZPR1F3b2NtY1dXb1VrWHJ0dz09'
         res.render('pages/index')
        })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
