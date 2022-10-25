b5a5a973aa9c0c193943b917fc462bc87b9f4e51
dcristobal-sf <44408128+dcristobal-sf@users.noreply.github.com>
2022-10-25 13:14:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-locals.token = 1234
+res.locals.token = 1234
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
