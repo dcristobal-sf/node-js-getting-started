e33fcb814146cd9131d0b909cc5057061f6af7cf
dcristobal-sf <44408128+dcristobal-sf@users.noreply.github.com>
2022-10-25 13:09:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+locals.token = 1234
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index',locals))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
